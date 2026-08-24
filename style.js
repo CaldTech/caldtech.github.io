dbf31df479c37aff75ed6b80e4c445900edbec00
Jordon Calder <36817483+CaldTech@users.noreply.github.com>
2026-08-24 17:24:15 -0500

    Update style.js

--- style.js
+++ style.js
@@ -1,6 +1,6 @@
 function toggleMenu(){
   let nav = document.getElementById("nav");
-  if(nav.style.display === "flex" || nav.style.display === "block"){
+  if(nav.style.display === "flex"){
     nav.style.display = "none";
   } else {
     nav.style.display = "flex";
